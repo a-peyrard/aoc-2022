a85fd5926811e30c95abde898391a1b8d78d5b46
Augustin Peyrard <augustin.peyrard@gmail.com>
2022-12-07 22:11:26 -0800

    play with multiple test cases

--- pkg/sandbox/addition_test.go
+++ pkg/sandbox/addition_test.go
@@ -4,20 +4,21 @@ import (
 	"testing"
 )
 
-func TestAdder(t *testing.T) {
-	sum := Add(2, 2)
-	expected := 4
+func TestAdd(t *testing.T) {
+	t.Run("should addition 2 and 2", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
 
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
-	}
-}
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
+	t.Run("should addition 2 and 23", func(t *testing.T) {
+		sum := Add(2, 3)
+		expected := 5
 
-func TestAdder2(t *testing.T) {
-	sum := Add(2, 3)
-	expected := 5
-
-	if sum != expected {
-		t.Errorf("expected '%d' but got '%d'", expected, sum)
-	}
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
 }
