0ce1113f695738ce10137c8f7e308aefc1ecedf2
Augustin Peyrard <augustin.peyrard@gmail.com>
2022-12-22 17:39:48 -0800

    feat(solution): day3 part1

---
+++ pkg/day3/rucksack_reorganization_test.go
@@ -0,0 +1,77 @@
+package day3
+
+import "testing"
+
+func TestDoSolution1(t *testing.T) {
+	type args struct {
+		raw string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"it should return the provided example result",
+			args{`vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`},
+			157,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DoSolution1(tt.args.raw); got != tt.want {
+				t.Errorf("DoSolution1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_priority(t *testing.T) {
+	type args struct {
+		item byte
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"it should return correct priority for 'P'",
+			args{'P'},
+			42,
+		},
+		{
+			"it should return correct priority for 't'",
+			args{'t'},
+			20,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := priority(tt.args.item); got != tt.want {
+				t.Errorf("priority() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolution1(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"result for solution1", 8072},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Solution1(); got != tt.want {
+				t.Errorf("Solution1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
