0c6f0c51ac71842ff7604ef368e486bf049b6b9f
Augustin Peyrard <augustin.peyrard@gmail.com>
2022-12-24 21:59:00 -0800

    feat(solution): day5 part2
    Didn't find a way to pass a struct method as argument :/

--- pkg/day5/supply_stacks_test.go
+++ pkg/day5/supply_stacks_test.go
@@ -49,3 +49,51 @@ func TestSolution1(t *testing.T) {
 		})
 	}
 }
+
+func TestDoSolution2(t *testing.T) {
+	type args struct {
+		raw string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"it should return the provided example result",
+			args{`    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2`},
+			"MCD",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DoSolution2(tt.args.raw); got != tt.want {
+				t.Errorf("DoSolution2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolution2(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"result for solution2", "SSCGWJCRB"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Solution2(); got != tt.want {
+				t.Errorf("Solution2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
