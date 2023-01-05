7594ddac231bfab6c177ffa9175ded06b2c3130b
Augustin Peyrard <augustin.peyrard@gmail.com>
2023-01-05 15:37:31 -0800

    feat(solution): adds day10 part2
    What a good idea to have the drawing library :D

--- pkg/day10/cathode_ray_tube_test.go
+++ pkg/day10/cathode_ray_tube_test.go
@@ -186,3 +186,198 @@ func TestSolution1(t *testing.T) {
 		})
 	}
 }
+
+func Test_doSolution2(t *testing.T) {
+	type args struct {
+		raw string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			"it should return the provided example result",
+			args{`addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop`},
+			`##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := doSolution2(tt.args.raw); got != tt.want {
+				t.Errorf("doSolution2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolution2(t *testing.T) {
+	tests := []struct {
+		name string
+		want string
+	}{
+		{"result for solution1", `###...##..####.####.#..#.#..#.###..#..#.
+#..#.#..#....#.#....#..#.#..#.#..#.#.#..
+#..#.#......#..###..####.#..#.#..#.##...
+###..#.##..#...#....#..#.#..#.###..#.#..
+#.#..#..#.#....#....#..#.#..#.#.#..#.#..
+#..#..###.####.####.#..#..##..#..#.#..#.`}, // RGZEHURK
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Solution2(); got != tt.want {
+				t.Errorf("Solution2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
