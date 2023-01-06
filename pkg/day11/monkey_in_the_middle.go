3930794d836a8eb261d97a6fa95c4225aea719f2
Augustin Peyrard <augustin.peyrard@gmail.com>
2023-01-05 22:36:46 -0800

    feat(solution): day11 part2
    had to use GCD to avoid number overflows, I tried first to play with
    big ints, but it was way too slow, and I totally forgot about this
    GCD trick (this is ringing a bell, I suppose there was a similar
    trick in a previous year)

--- pkg/day11/monkey_in_the_middle.go
+++ pkg/day11/monkey_in_the_middle.go
@@ -233,6 +233,92 @@ situation can be found by multiplying these together: 10605. Figure out which mo
 items they inspect over 20 rounds. What is the level of monkey business after 20 rounds of stuff-slinging simian
 shenanigans?
 
+--- Part Two ---
+
+You're worried you might not ever get your items back. So worried, in fact, that your relief that a monkey's inspection
+didn't damage an item no longer causes your worry level to be divided by three. Unfortunately, that relief was all that
+was keeping your worry levels from reaching ridiculous levels. You'll need to find another way to keep your worry levels
+manageable. At this rate, you might be putting up with these monkeys for a very long time - possibly 10000 rounds! With
+these new rules, you can still figure out the monkey business after 10000 rounds. Using the same example above:
+
+== After round 1 ==
+Monkey 0 inspected items 2 times.
+Monkey 1 inspected items 4 times.
+Monkey 2 inspected items 3 times.
+Monkey 3 inspected items 6 times.
+
+== After round 20 ==
+Monkey 0 inspected items 99 times.
+Monkey 1 inspected items 97 times.
+Monkey 2 inspected items 8 times.
+Monkey 3 inspected items 103 times.
+
+== After round 1000 ==
+Monkey 0 inspected items 5204 times.
+Monkey 1 inspected items 4792 times.
+Monkey 2 inspected items 199 times.
+Monkey 3 inspected items 5192 times.
+
+== After round 2000 ==
+Monkey 0 inspected items 10419 times.
+Monkey 1 inspected items 9577 times.
+Monkey 2 inspected items 392 times.
+Monkey 3 inspected items 10391 times.
+
+== After round 3000 ==
+Monkey 0 inspected items 15638 times.
+Monkey 1 inspected items 14358 times.
+Monkey 2 inspected items 587 times.
+Monkey 3 inspected items 15593 times.
+
+== After round 4000 ==
+Monkey 0 inspected items 20858 times.
+Monkey 1 inspected items 19138 times.
+Monkey 2 inspected items 780 times.
+Monkey 3 inspected items 20797 times.
+
+== After round 5000 ==
+Monkey 0 inspected items 26075 times.
+Monkey 1 inspected items 23921 times.
+Monkey 2 inspected items 974 times.
+Monkey 3 inspected items 26000 times.
+
+== After round 6000 ==
+Monkey 0 inspected items 31294 times.
+Monkey 1 inspected items 28702 times.
+Monkey 2 inspected items 1165 times.
+Monkey 3 inspected items 31204 times.
+
+== After round 7000 ==
+Monkey 0 inspected items 36508 times.
+Monkey 1 inspected items 33488 times.
+Monkey 2 inspected items 1360 times.
+Monkey 3 inspected items 36400 times.
+
+== After round 8000 ==
+Monkey 0 inspected items 41728 times.
+Monkey 1 inspected items 38268 times.
+Monkey 2 inspected items 1553 times.
+Monkey 3 inspected items 41606 times.
+
+== After round 9000 ==
+Monkey 0 inspected items 46945 times.
+Monkey 1 inspected items 43051 times.
+Monkey 2 inspected items 1746 times.
+Monkey 3 inspected items 46807 times.
+
+== After round 10000 ==
+Monkey 0 inspected items 52166 times.
+Monkey 1 inspected items 47830 times.
+Monkey 2 inspected items 1938 times.
+Monkey 3 inspected items 52013 times.
+
+After 10000 rounds, the two most active monkeys inspected items 52166 and 52013 times. Multiplying these together, the
+level of monkey business in this situation is now 2713310158. Worry levels are no longer divided by three after each
+item is inspected; you'll need to find another way to keep your worry levels manageable. Starting again from the initial
+state in your puzzle input, what is the level of monkey business after 10000 rounds?
+
+
 */
 
 type sign byte
@@ -274,14 +360,18 @@ func (m *monkey) receive(item int) {
 	m.items.Push(item)
 }
 
-func (m *monkey) inspect() (bool, int, int8) {
+func (m *monkey) inspect(relief bool, reliever int) (bool, int, int8) {
 	if m.items.IsEmpty() {
 		return false, -1, -1
 	}
 	m.inspections += 1
 	item := m.items.Pop()          // pick up an item
 	item = m.operation.apply(item) // apply stress operation
-	item /= 3                      // get bored
+	if relief {
+		item /= 3 // get bored
+	} else {
+		item %= reliever // avoid number too big
+	}
 
 	// check who will be receiver of item
 	var throwTo int8
@@ -294,7 +384,7 @@ func (m *monkey) inspect() (bool, int, int8) {
 	return true, item, throwTo
 }
 
-func doRound(monkeys []*monkey) {
+func doRound(monkeys []*monkey, relief bool, reliever int) {
 	var (
 		hasItem bool
 		item    int
@@ -303,7 +393,7 @@ func doRound(monkeys []*monkey) {
 	for _, monkey := range monkeys {
 		hasItem = true
 		for hasItem {
-			hasItem, item, throwTo = monkey.inspect()
+			hasItem, item, throwTo = monkey.inspect(relief, reliever)
 			if hasItem {
 				monkeys[throwTo].receive(item)
 			}
@@ -386,10 +476,14 @@ func parseOperation(rawOperation string) operation {
 	}
 }
 
-func doSolution1(raw string) int {
+func doSolutionGen(raw string, relief bool, rounds int) int {
 	monkeys := parse(raw)
-	for i := 0; i < 20; i++ {
-		doRound(monkeys)
+	reliever := 1
+	for _, monkey := range monkeys {
+		reliever *= monkey.test
+	}
+	for i := 0; i < rounds; i++ {
+		doRound(monkeys, relief, reliever)
 	}
 
 	max1, max2 := 0, 0
@@ -404,6 +498,18 @@ func doSolution1(raw string) int {
 	return max1 * max2
 }
 
+func doSolution1(raw string) int {
+	return doSolutionGen(raw, true, 20)
+}
+
 func Solution1() int {
 	return doSolution1(util.GetInputContent())
+}
+
+func doSolution2(raw string) int {
+	return doSolutionGen(raw, false, 10_000)
+}
+
+func Solution2() int {
+	return doSolution2(util.GetInputContent())
 }
--- pkg/day11/monkey_in_the_middle_test.go
+++ pkg/day11/monkey_in_the_middle_test.go
@@ -2,18 +2,7 @@ package day11
 
 import "testing"
 
-func Test_doSolution1(t *testing.T) {
-	type args struct {
-		raw string
-	}
-	tests := []struct {
-		name string
-		args args
-		want int
-	}{
-		{
-			"it should return the provided example result",
-			args{`Monkey 0:
+const InputExample = `Monkey 0:
   Starting items: 79, 98
   Operation: new = old * 19
   Test: divisible by 23
@@ -39,7 +28,20 @@ Monkey 3:
   Operation: new = old + 3
   Test: divisible by 17
     If true: throw to monkey 0
-    If false: throw to monkey 1`},
+    If false: throw to monkey 1`
+
+func Test_doSolution1(t *testing.T) {
+	type args struct {
+		raw string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"it should return the provided example result",
+			args{InputExample},
 			10605,
 		},
 	}
@@ -63,6 +65,82 @@ func TestSolution1(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Solution1(); got != tt.want {
 				t.Errorf("Solution1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_doSolution2(t *testing.T) {
+	type args struct {
+		raw string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"it should return the provided example result",
+			args{InputExample},
+			2713310158,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := doSolution2(tt.args.raw); got != tt.want {
+				t.Errorf("doSolution2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSolution2(t *testing.T) {
+	tests := []struct {
+		name string
+		want int
+	}{
+		{"result for solution2", 14952185856},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Solution2(); got != tt.want {
+				t.Errorf("Solution2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_doSolutionGen(t *testing.T) {
+	type args struct {
+		raw    string
+		relief bool
+		rounds int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			"it should return the provided example after 1 round",
+			args{InputExample, false, 1},
+			24,
+		},
+		{
+			"it should return the provided example after 20 round",
+			args{InputExample, false, 20},
+			10197,
+		},
+		{
+			"it should return the provided example after 1000 round",
+			args{InputExample, false, 1000},
+			27019168,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := doSolutionGen(tt.args.raw, tt.args.relief, tt.args.rounds); got != tt.want {
+				t.Errorf("doSolutionGen() = %v, want %v", got, tt.want)
 			}
 		})
 	}
